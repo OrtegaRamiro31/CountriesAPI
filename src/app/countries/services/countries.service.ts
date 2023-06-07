34751fae5d83cb5c9ce5012b2e2c2f9513b03954
ramiro <ceinnova13458@innovaccion.mx>
2023-06-07 14:54:38 -0600

    Agregado resultado de búsqueda por capital

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -4,13 +4,13 @@ import { Observable } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
-export class ServiceNameService {
+export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
 
   searchCapital(term: string): Observable<Country[]> {
-    const url: string = `${this.apiUrl}/caìtal/${term}`;
+    const url: string = `${this.apiUrl}/capital/${term}`;
     return this.http.get<Country[]>(url);
   }
 }
