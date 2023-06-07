326f5fc9f59931acdf55105c7a462fa1a3291141
ramiro <ceinnova13458@innovaccion.mx>
2023-06-07 12:11:55 -0600

    Añadido Sidebar con navegación

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, HomePageComponent],
-  imports: [CommonModule],
-  exports: [AboutPageComponent, HomePageComponent],
+  declarations: [AboutPageComponent, HomePageComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [AboutPageComponent, HomePageComponent, SidebarComponent],
 })
 export class SharedModule {}
