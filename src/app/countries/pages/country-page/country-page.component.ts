06fa1be6bce0399a5cff85eca058580a2946a921
ramiro <ceinnova13458@innovaccion.mx>
2023-06-07 16:58:40 -0600

    Información de cada país ahora es visible cuando se usa la ruta by/:id

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: [],
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private countriesService: CountriesService,
@@ -22,11 +25,8 @@ export class CountryPageComponent implements OnInit {
         )
       )
       .subscribe((country) => {
-        if (!country) {
-          return this.router.navigateByUrl('');
-        }
-        console.log('Tenemos país');
-        return;
+        if (!country) return this.router.navigateByUrl('');
+        return (this.country = country);
       });
   }
 }
