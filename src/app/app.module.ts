2e90e0aed0e06c412dd7bc007d8a482df74dc85f
ramiro <ceinnova13458@innovaccion.mx>
2023-06-07 14:40:55 -0600

    Agregado countries.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, SharedModule],
+  imports: [AppRoutingModule, BrowserModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
