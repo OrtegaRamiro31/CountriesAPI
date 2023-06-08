29c4d6e9bffff47ba1e261b1b4eed17f500eca46
ramiro <ceinnova13458@innovaccion.mx>
2023-06-08 11:59:02 -0600

    Unsuscribe realizado en el método ngOnDestroy del componente search-box para cancelar la suscripción del Subject

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,13 +1,21 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: [],
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -19,9 +27,17 @@ export class SearchBoxComponent implements OnInit {
   public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer.pipe(debounceTime(300)).subscribe((value) => {
-      this.onDebounce.emit(value);
-    });
+    console.log('Inicializado');
+    this.debouncerSuscription = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destruido');
+    this.debouncerSuscription?.unsubscribe;
   }
 
   emitValue(value: string): void {
