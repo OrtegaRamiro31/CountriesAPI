a20afd9e1b00e38d06b236dfeaccf163a19599fb
ramiro <ceinnova13458@innovaccion.mx>
2023-06-08 14:20:30 -0600

    Cambios de estilos realizados. Build realizado

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: [],
+  styles: [
+    `
+      .custom-list-item {
+        border: 1px solid rgba(0, 0, 0, 0.125);
+        border-radius: 5px;
+      }
+    `,
+  ],
 })
 export class SidebarComponent {}
